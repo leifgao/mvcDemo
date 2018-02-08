70e9c0f06a581c70f285b5b2b6e99c4b3f1a00be
leifgao <leifgao@outlook.com>
2018-02-08 22:00:08 +0800

    配置阿里maven仓库，配置slf4j+log4j2日志

--- p38/src/main/java/me/leifgao/www/controller/HelloWorldController.java
+++ p38/src/main/java/me/leifgao/www/controller/HelloWorldController.java
@@ -1,6 +1,8 @@
 package me.leifgao.www.controller;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
 
@@ -11,12 +13,16 @@ import javax.servlet.http.HttpServletResponse;
  * Created by leif on 2018/2/5
  */
 public class HelloWorldController implements Controller {
+
+    private static final Logger logger = LoggerFactory.getLogger(HelloWorldController.class);
+
     @Override
     public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
-
+        logger.info("--------> start 开始");
         ModelAndView mv = new ModelAndView();
         mv.addObject("message", "hello world");
         mv.setViewName("hello");
+        logger.info("--------> end 结束");
         return mv;
     }
 }
